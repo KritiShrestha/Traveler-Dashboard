a04bad56fb8d8704c75263a1ba7286109f42e3d4
Nephew20 <cofieldk20@gmail.com>
2023-03-07 20:43:34 -0500

    weather data

--- assets/js/fetch.js
+++ assets/js/fetch.js
@@ -1,5 +1,6 @@
 function getWeatherApi () {
-    var requestUrl = 'https://api.open-meteo.com/v1/forecast?latitude=35.02&longitude=-79.02&hourly=temperature_2m,relativehumidity_2m,windspeed_10m&daily=weathercode,sunrise,sunset,precipitation_probability_max&current_weather=true&temperature_unit=fahrenheit&timezone=auto';
+   // var requestUrl = 'https://api.open-meteo.com/v1/forecast?latitude=35.02&longitude=-79.02&hourly=temperature_2m,relativehumidity_2m,windspeed_10m&daily=weathercode,sunrise,sunset,precipitation_probability_max&current_weather=true&temperature_unit=fahrenheit&timezone=auto';
+    var requestUrl = 'https://api.open-meteo.com/v1/forecast?latitude=35.77&longitude=-78.64&daily=weathercode,temperature_2m_max,apparent_temperature_max,sunrise,sunset,precipitation_probability_max&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York'
 
     fetch(requestUrl)
         .then(function(response) {
@@ -16,10 +17,43 @@ function getWeatherApi () {
 function appendWeather(data) {
     var weather = $('.weather-forecast');
 
+    // ID Variables 
     var temp = $('#temp');
+    var wind_speed = $('#wind-speed')
+    var wind_direct = $('#wind-direction')
 
-    temp.text(data.current_weather.temperature);
+    //Weather Values
+    temp.text(data.current_weather.temperature + " F");
+    wind_speed.text(data.current_weather.windspeed + " mph")
+    
+    var wind_spot = data.current_weather.winddirection
+    console.log(wind_spot)
+
+    // finding wind direction 
+    if (wind_spot > 0 && wind_spot < 90) {
+        wind_direct.text('Wind Direction: NE')
+    } else if (wind_spot > 90 && wind_spot < 180) {
+        wind_direct.text('Wind Direction: SE')
+    } else if (wind_spot > 180 && wind_spot < 270) {
+        wind_direct.text('Wind Direction: SW') 
+    } else if (wind_spot > 270 && wind_spot < 360) {
+        wind_direct.text('Wind Direction: NW')
+    } else if (wind_spot === 0 || wind_spot === 360) {
+        wind_direct.text('Wind Direction: N')
+    } else if (wind_spot === 90) {
+        wind_direct.text('Wind Direction: E')
+    } else if (wind_spot === 180) {
+        wind_direct.text('Wind Direction: S')
+    } else if (wind_spot === 270) {
+        wind_direct.text('Wind Direction: W')
+    }   
+
+    // weather img
+    weatherPic = $('#weather-pic')
+    
+    
 }
+
 
 function getMapApi() {
     //console.log(data.latitude, data.longitude);
